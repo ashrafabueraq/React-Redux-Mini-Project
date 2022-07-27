b835d2052d426f66a947372f8d7418c306b2d274
ashrafabueraq <ashrafabueraq97@gmail.com>
2022-07-27 16:02:55 +0300

    main

---
+++ Redux-react/src/components/footer.js
@@ -0,0 +1,92 @@
+
+
+const Footer = ()=>{
+    return(
+
+      <footer className="text-center text-white" style={{backgroundColor:"#f1f1f1"}}>
+     
+      <div class="container pt-4">
+       
+        <section class="mb-4">
+        
+          <a
+            class="btn btn-link btn-floating btn-lg text-dark m-1"
+            href="#!"
+            role="button"
+            data-mdb-ripple-color="dark"
+            ><i class="fab fa-facebook-f"></i
+          ></a>
+    
+          
+          <a
+            class="btn btn-link btn-floating btn-lg text-dark m-1"
+            href="#!"
+            role="button"
+            data-mdb-ripple-color="dark"
+            ><i class="fab fa-twitter"></i
+          ></a>
+    
+        
+          <a
+            class="btn btn-link btn-floating btn-lg text-dark m-1"
+            href="#!"
+            role="button"
+            data-mdb-ripple-color="dark"
+            ><i class="fab fa-google"></i
+          ></a>
+    
+          
+          <a
+            class="btn btn-link btn-floating btn-lg text-dark m-1"
+            href="#!"
+            role="button"
+            data-mdb-ripple-color="dark"
+            ><i class="fab fa-instagram"></i
+          ></a>
+    
+         
+          <a
+            class="btn btn-link btn-floating btn-lg text-dark m-1"
+            href="#!"
+            role="button"
+            data-mdb-ripple-color="dark"
+            ><i class="fab fa-linkedin"></i
+          ></a>
+        
+          <a
+            class="btn btn-link btn-floating btn-lg text-dark m-1"
+            href="#!"
+            role="button"
+            data-mdb-ripple-color="dark"
+            ><i class="fab fa-github"></i
+          ></a>
+        </section>
+   
+      </div>
+
+      <div className="container">
+        <p style={{color:"black"}}>
+        Find and Add Any of Our Free Icons to a Project
+        Browse the thousands of icons to find some icons you like and then add the icon name and style as CSS classes to an HTML
+        Find and Add Any of Our Free Icons to a Project
+        Browse the thousands of icons to find some icons you like and then add the icon name and style as CSS classes to an HTML
+        Find and Add Any of Our Free Icons to a Project
+        Browse the thousands of icons to find some icons you like and then add the icon name and style as CSS classes to an HTML
+        Find and Add Any of Our Free Icons to a Project
+        Browse the thousands of icons 
+        </p>
+      </div>
+     
+    
+      
+      <div class="text-center text-dark p-3" style={{backgroundColor:"rgba(0, 0, 0, 0.2)"}}>
+        © 2020 Copyright:
+        <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+      </div>
+     
+    </footer>
+
+    );
+}
+
+export default Footer;
